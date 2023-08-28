d529e527663326e1a339a08326a5b39e7cfa1181
Tácio Soares <taciofsoares@gmail.com>
2023-08-28 13:29:09 -0300

    ok

---
+++ caracteristicasdolocal/logica/mostrarRespostas.js
@@ -0,0 +1,19 @@
+
+var botaoDeResposta = document.querySelectorAll('.chamaResposta')
+
+for (let i = 0; i < botaoDeResposta.length; i++) {
+    botaoDeResposta[i].addEventListener('click', function(){
+        
+        if (botaoDeResposta[i].value == 'Resposta') {
+            this.parentNode.nextElementSibling.style.animation = 'mostrar2 1s ease-in-out forwards'
+            this.parentNode.nextElementSibling.style.display = 'block'
+            botaoDeResposta[i].value = 'Ocultar'
+        } else {
+            this.parentNode.nextElementSibling.style.animation = 'ocultar 1s ease-in-out forwards'
+            setTimeout(() => {
+                this.parentNode.nextElementSibling.style.display = 'none'
+                botaoDeResposta[i].value = 'Resposta'
+            }, 900);
+        }    
+    })    
+}
